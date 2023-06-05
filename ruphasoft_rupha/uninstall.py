a5fdaf4e6902b59bdf455cec62ec02eb6cc8c1ab
Mohamud <mohamud.a.a@gmail.com>
2023-06-05 15:00:50 +0300

    fix: icon set install fails on wrong home env var

--- ruphasoft_rupha/uninstall.py
+++ ruphasoft_rupha/uninstall.py
@@ -2,11 +2,16 @@ import os
 
 HOME = os.environ.get('HOME')
 
+
 def before_uninstall():
     print("Before Uninstall")
     print("=====================================")
     print("removing icons from icons.svg")
-    os.remove("/home/"+HOME+"/frappe-bench/apps/frappe/frappe/public/icons/timeless/icons.svg")
-    os.rename("/home/"+HOME+"/frappe-bench/apps/frappe/frappe/public/icons/timeless/icons.bak.svg", "/home/"+HOME+"/frappe-bench/apps/frappe/frappe/public/icons/timeless/icons.svg")
+    try:
+        os.remove(HOME+"/frappe-bench/apps/frappe/frappe/public/icons/timeless/icons.svg")
+        os.rename(HOME+"/frappe-bench/apps/frappe/frappe/public/icons/timeless/icons.bak.svg", "/home/"+HOME+"/frappe-bench/apps/frappe/frappe/public/icons/timeless/icons.svg")
+    except Exception as e:
+        print("============ERROR==================")
+        print(e)
     print("=====================================")
     print("Before Uninstall DONE!")
